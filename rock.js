36ba53db2dc9707d974895ba9c1e35ef371e1066
neal <nealwilson@live.co.uk>
2022-06-03 21:51:22 +0100

    first attempt at making the buttons play the game

--- rock.js
+++ rock.js
@@ -1,45 +1,97 @@
 
 
-//generates a number from 1 - 3
-    Math.random() 
-    let randomNumber = Math.floor(Math.random() * 3) +1;
-    
-
-console.log(randomNumber) //test purpose
-
-//convert randomNumber into rock,paper,siscs
-
-let computerPlays;
-
-if (randomNumber === 1) {
-    computerPlays = "rock";
-}
-else if (randomNumber === 2) {
-    computerPlays = "paper";
-}
-else {
-    computerPlays = "siscors";
+function computerPlays() {
+    let random = Math.random();
+    if (random <= 0.3333) {
+        return "paper";
+    } else if (random >= 0.6666) {
+        return "rock";
+    } else {
+        return "scissors";
+    }
 }
 
-console.log(randomNumber) //test
-console.log(computerPlays) //test
-
-
-let playerPrompt = prompt("Please enter rock, paper or siscors: ")
-
-console.log(playerPrompt)
+let computerScore = parseInt(0);
+let playerScore = parseInt(0);
+let win = "You win!"
+let lose = "You Lose!"
+let tie = "Its a tie!"
 
 let playerSelection;
 
-if (playerPrompt === "rock") {
-    let playerSelection = 1
-}
-else if (playerPrompt === "paper") {
-    let playerSelection = 2
-}
-else {
-    let playerSelection = 3
+const rockB = document.getElementById("rockButton");
+    rockB.addEventListener("click", function(e) {
+        console.log("rock");
+        let playerSelection = "rock";
+    }, playRound()
+    );
+
+
+const paperB = document.getElementById("paperButton");
+    paperB.addEventListener("click", function (e) {
+        console.log("paper")
+        let playerSelection = "paper"
+    }, playRound());
+
+
+const siscorsB = document.getElementById("siscorsButton");
+    siscorsB.addEventListener("click", function(e) {
+        console.log("siscors")
+        let playerSelection = "siscors"
+    }, playRound());
+    
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            computerScore++;
+            return lose;
+        }
+        } else if (computerSelection === "rock") {
+        return tie;
+        } else {
+        playerScore++
+        return win;
+        }
+    
+    if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            playerScore++
+            return win;
+        } else if (computerSelection === "paper") {
+            return tie;
+        } else {
+            computerScore++;
+            return lose;
+        }
+    }
+
+    if (playerSelection.toLowerCase() === "siscors") {
+        if (computerSelection === "paper") {
+            playerScore++;
+            return win;
+        } else if (computerSelection === "rock") {
+            computerScore++;
+            return lose;
+        } else {
+            return tie;
+        }
+    }
 }
 
-console.log(playerSelection)
 
+
+//var i = 0
+//const game = () => {
+  //  let playerSelection = prompt("Pick rock,paper or siscors")
+   // const computerSelection = computerPlays()
+   // console.log(playRound(playerSelection, computerSelection))
+   // console.log("your score = " + playerScore);
+   // console.log("Computer's score = " + computerScore);
+   // i++;
+   // if (i !== 5) { 
+    //    game()
+    //} else {
+      //  alert("Game over! Player Score :("+playerScore+") Computer Score : ("+computerScore+")");
+    //}
+//}
+//game();
